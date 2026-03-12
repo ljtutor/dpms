cca90cca297abafbfeffaee9ae3e1351a28309d3
Louie Jay Tutor <ljtutor@dataplus.com.ph>
2026-03-12 15:44:50 +0800

    updates in header

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <div className="flex flex-col items-center justify-center px-6 pt-8 mx-auto md:h-screen pt:mt-0 dark:bg-gray-900">
+
+    </div>
+  );
+}
