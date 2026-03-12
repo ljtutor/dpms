3973db0c92b20b7476f702db9aa71124461ee5e6
TGMics2 <100818214+TGMics2@users.noreply.github.com>
2026-03-12 15:49:25 +0800

    Template updates from main
    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,8 +36,8 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
         <meta charSet="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&amp;display=swap" rel="stylesheet"/>
-        <link rel="stylesheet" href="css/app.css"/>
-        <link rel="stylesheet" href="css/main.css"/>
+        <link rel="stylesheet" href="/css/app.css"/>
+        <link rel="stylesheet" href="/css/main.css"/>
       </head>
       <body className="bg-gray-50 dark:bg-gray-800">
         <LayoutClient>{children}</LayoutClient>
